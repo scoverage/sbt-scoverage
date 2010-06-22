04e7cf2a5f238d547770714ff0fe1a7da5247711
Mikko Koponen <mikko.koponen@reaktor.fi>
2010-06-23 01:39:38 +0300

    Separate projects, as sbt-plugin requires 2.7.7 and trying to get actual code-coverage tool to work with 2.8.

---
+++ project/build/Build.scala
@@ -0,0 +1,3 @@
+import sbt._
+
+class Build(info: ProjectInfo) extends PluginProject(info) with IdeaProject
---
+++ project/plugins/Plugins.scala
@@ -0,0 +1,7 @@
+import sbt._
+class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
+  val ideaPluginRepo = "GH-pages repo" at "http://mpeltonen.github.com/maven/"
+  val ideaPlugin = "com.github.mpeltonen" % "sbt-idea-plugin" % "0.1-SNAPSHOT"
+  // Self-test:
+  // val scctPlugin = "reaktor" % "sbt-scct" % "0.1-SNAPSHOT"
+}
