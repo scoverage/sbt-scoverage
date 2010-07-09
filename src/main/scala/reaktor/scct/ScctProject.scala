4e4c23f46c27540d41d609f15c20d0a51385b999
Mikko Koponen <mikko.koponen@reaktor.fi>
2010-07-10 02:59:47 +0300

    Eat my own dogfood. And add filtering to report packagelist.

--- src/main/scala/reaktor/scct/ScctProject.scala
+++ src/main/scala/reaktor/scct/ScctProject.scala
@@ -4,11 +4,9 @@ import sbt._
 import java.util.jar.Manifest
 
 trait ScctProject extends DefaultProject {
-  private lazy val pluginLibDir = rootProject.info.pluginsManagedDependencyPath / String.format("scala_%s", defScalaVersion.value)
-  def coverageSelfTest = false
   def scctPluginJar = testClasspath ** "scct_2.8.0.RC7-1.0.jar"
 
-  def testRuntimeScctPluginJar = if (!coverageSelfTest) scctPluginJar else outputPath / "scct-xml.jar"
+  def testRuntimeScctPluginJar = scctPluginJar
   def instrumentedClassDir = outputPath / "coverage-classes"
   def reportDir = outputPath / "coverage-report"
 
@@ -34,14 +32,8 @@ trait ScctProject extends DefaultProject {
   lazy val instrumentTestCompileConditional = new CompileConditional(instrumentTestCompileConfiguration, buildCompiler)
   lazy val instrumentCompileConditional = new CompileConditional(instrumentCompileConfiguration, buildCompiler)
 
-  protected def xmlJarAction = task {
-    if (coverageSelfTest) {
-      FileUtilities.jar(((mainResources / "scalac-plugin.xml").get), outputPath / "scct-xml.jar", new Manifest(), false, log)
-    }
-    None
-  }
   protected def instrumentAction =
-    task { instrumentCompileConditional.run } dependsOn xmlJar
+    task { instrumentCompileConditional.run }
   protected def testCoverageCompileAction =
     task { instrumentTestCompileConditional.run } dependsOn instrument
   protected def instrumentedTestRunClassPath =
@@ -52,14 +44,12 @@ trait ScctProject extends DefaultProject {
   protected def testCoverageAction =
     testTask(testFrameworks, instrumentedTestRunClassPath, instrumentTestCompileConditional.analysis, instrumentedTestOptions).dependsOn(testCoverageCompile, copyResources, copyTestResources, setupCoverageEnv)
 
-  lazy val xmlJar = xmlJarAction
   lazy val instrument = instrumentAction
   lazy val testCoverage = testCoverageAction
   lazy val testCoverageCompile = testCoverageCompileAction
   lazy val setupCoverageEnv = task {
     if (reportDir.exists) FileUtilities.clean(reportDir, log)
     FileUtilities.createDirectory(reportDir, log)
-    if (coverageSelfTest) System.setProperty("scct-self-test", true.toString)
     System.setProperty("scct.report.dir", reportDir.toString)
     System.setProperty("scct.src.reference.dir", mainScalaSourcePath.absolutePath)
     None
