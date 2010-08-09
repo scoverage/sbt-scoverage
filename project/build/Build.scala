905243c806a1744b3f4e5efb2c8d7a95b0c8dcaf
Mikko Koponen <mikko.koponen@reaktor.fi>
2010-08-09 23:56:09 +0300

    Fix version back to 0.1-SNAPSHOT, and use local maven repo for publishing.

--- project/build/Build.scala
+++ project/build/Build.scala
@@ -1,5 +1,8 @@
 import sbt._
 
 class Build(info: ProjectInfo) extends PluginProject(info) with IdeaProject {
-  val scct = "reaktor" % "scct_2.7.7" % "1.0"
+	override def managedStyle = ManagedStyle.Maven
+	lazy val publishTo = Resolver.file("github-pages-repo", new java.io.File("../maven-repo/"))
+
+  val scct = "reaktor" % "scct_2.7.7" % "0.1-SNAPSHOT"
 }
