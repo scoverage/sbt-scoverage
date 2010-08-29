10785528573ca345399b08dbe64e6315099d5926
Mikko Koponen <mikko.koponen@reaktor.fi>
2010-08-29 23:45:31 +0300

    Fix local gh-pages checkout location.

--- project/build/Build.scala
+++ project/build/Build.scala
@@ -2,7 +2,7 @@ import sbt._
 
 class Build(info: ProjectInfo) extends PluginProject(info) with IdeaProject {
 	override def managedStyle = ManagedStyle.Maven
-	lazy val publishTo = Resolver.file("github-pages-repo", new java.io.File("../maven-repo/"))
+	lazy val publishTo = Resolver.file("github-pages-repo", new java.io.File("../../gh-pages/maven-repo/"))
 
   val scct = "reaktor" % "scct_2.8.0" % "0.1-SNAPSHOT"
 }
