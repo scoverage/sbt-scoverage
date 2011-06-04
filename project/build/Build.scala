b1e7ba025ff7d4fb64be15ac52216949240ec42c
Mikko Koponen <mikko.koponen@reaktor.fi>
2011-06-04 16:01:44 +0300

    Upgrade scala 2.9 --> 2.9.0-1

--- project/build/Build.scala
+++ project/build/Build.scala
@@ -4,5 +4,5 @@ class Build(info: ProjectInfo) extends PluginProject(info) with IdeaProject {
 	override def managedStyle = ManagedStyle.Maven
 	lazy val publishTo = Resolver.file("github-pages-repo", new java.io.File("../../gh-pages/maven-repo/"))
 
-  val scct = "reaktor" % "scct_2.8.0" % "0.1-SNAPSHOT"
+  val scct = "reaktor" % "scct_2.9.0" % "0.1-SNAPSHOT"
 }
