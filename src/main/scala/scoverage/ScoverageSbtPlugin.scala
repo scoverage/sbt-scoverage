a0e8b41c1a3862da4115dffa5597a071df53698c
sksamuel <sam@sksamuel.com>
2014-07-20 11:21:19 +0100

    Updated itest configs

--- src/main/scala/scoverage/ScoverageSbtPlugin.scala
+++ src/main/scala/scoverage/ScoverageSbtPlugin.scala
@@ -35,13 +35,6 @@ class ScoverageSbtPlugin extends sbt.Plugin {
         libraryDependencies += {
           OrgScoverage % (ScalacArtifact + "_" + scalaBinaryVersion.value) % ScoverageVersion % ScoverageCompile.name
         },
-        sources in ScoverageCompile <<= (sources in Compile),
-        sourceDirectory in ScoverageCompile <<= (sourceDirectory in Compile),
-        resourceDirectory in ScoverageCompile <<= (resourceDirectory in Compile),
-        resourceGenerators in ScoverageCompile <<= (resourceGenerators in Compile),
-        excludedPackages in ScoverageCompile := "",
-        javacOptions in ScoverageCompile <<= (javacOptions in Compile),
-        javaOptions in ScoverageCompile <<= (javaOptions in Compile),
 
         minimumCoverage := 0, // default is no minimum
         failOnMinimumCoverage := false,
@@ -72,9 +65,9 @@ class ScoverageSbtPlugin extends sbt.Plugin {
 
           val dataDir = cross / "/scoverage-data"
           val coberturaDir = cross / "coverage-report"
-          val reportDir = cross / "scoverage-report"
+          val scoverageDir = cross / "scoverage-report"
           coberturaDir.mkdirs()
-          reportDir.mkdirs()
+          scoverageDir.mkdirs()
 
           val coverageFile = IOUtils.coverageFile(dataDir)
           val measurementFiles = IOUtils.findMeasurementFiles(dataDir)
@@ -89,27 +82,26 @@ class ScoverageSbtPlugin extends sbt.Plugin {
           log.info(s"[scoverage] Generating Cobertura report [${coberturaDir.getAbsolutePath}/cobertura.xml]")
           new CoberturaXmlWriter(baseDir, coberturaDir).write(coverage)
 
-          log.info(s"[scoverage] Generating XML report [${reportDir.getAbsolutePath}/scoverage.xml]")
-          new ScoverageXmlWriter(compileSourceDirectory, reportDir, false).write(coverage)
-          new ScoverageXmlWriter(compileSourceDirectory, reportDir, true).write(coverage)
+          log.info(s"[scoverage] Generating XML report [${scoverageDir.getAbsolutePath}/scoverage.xml]")
+          new ScoverageXmlWriter(compileSourceDirectory, scoverageDir, false).write(coverage)
+          new ScoverageXmlWriter(compileSourceDirectory, scoverageDir, true).write(coverage)
 
-          log.info(s"[scoverage] Generating HTML report [${reportDir.getAbsolutePath}/index.html]")
-          new ScoverageHtmlWriter(compileSourceDirectory, reportDir).write(coverage)
+          log.info(s"[scoverage] Generating HTML report [${scoverageDir.getAbsolutePath}/index.html]")
+          new ScoverageHtmlWriter(compileSourceDirectory, scoverageDir).write(coverage)
 
           log.info("[scoverage] Reports completed")
           val min = minimumCoverage.value
-          val failOnMin = failOnMinimumCoverage.value
 
           // check for default minimum
           if (min > 0) {
             if (min > coverage.statementCoveragePercent) {
-              log
-                .error(s"[scoverage] Coverage is below minimum [${coverage.statementCoverageFormatted}% < $min%]")
-              if (failOnMin)
-                throw new RuntimeException("Coverage minimum was not reached")
+              log.error(s"[scoverage] Coverage is below minimum [${coverage.statementCoverageFormatted}% < $min%]")
+              if (failOnMinimumCoverage.value) {
+                // todo better way to fail an SBT build ?
+                throw new RuntimeException("Coverage minimum was not reached. Failing build.")
+              }
             } else {
-              log
-                .info(s"[scoverage] Coverage is above minimum [${coverage.statementCoverageFormatted}% > $min%]")
+              log.info(s"[scoverage] Coverage is above minimum [${coverage.statementCoverageFormatted}% > $min%]")
             }
           }
 
@@ -118,39 +110,49 @@ class ScoverageSbtPlugin extends sbt.Plugin {
 
         scalacOptions in ScoverageCompile ++= (if (highlighting.value) List("-Yrangepos") else Nil),
 
+        sources in ScoverageCompile <<= (sources in Compile),
+        sourceDirectory in ScoverageCompile <<= (sourceDirectory in Compile),
+        resourceDirectory in ScoverageCompile <<= (resourceDirectory in Compile),
+        resourceGenerators in ScoverageCompile <<= (resourceGenerators in Compile),
+        unmanagedResources in ScoverageCompile <<= (unmanagedResources in Compile),
+        javaOptions in ScoverageCompile <<= (javaOptions in Compile),
+        javacOptions in ScoverageCompile <<= (javacOptions in Compile),
+        fork in ScoverageCompileTest <<= (fork in Compile),
+        excludedPackages in ScoverageCompile := "",
+
         sources in ScoverageTest <<= (sources in Test),
         sourceDirectory in ScoverageTest <<= (sourceDirectory in Test),
         resourceDirectory in ScoverageTest <<= (resourceDirectory in Test),
         resourceGenerators in ScoverageTest <<= (resourceGenerators in Test),
         unmanagedResources in ScoverageTest <<= (unmanagedResources in Test),
-        javacOptions in ScoverageTest <<= (javacOptions in Test),
         javaOptions in ScoverageTest <<= (javaOptions in Test),
+        javacOptions in ScoverageTest <<= (javacOptions in Test),
         fork in ScoverageTest <<= (fork in Test),
         testOptions in ScoverageTest <<= (testOptions in Test),
-        sources in ScoverageITest <<= (sources in Test),
 
-        sourceDirectory in ScoverageITest <<= (sourceDirectory in Test),
-        resourceDirectory in ScoverageITest <<= (resourceDirectory in Test),
-        resourceGenerators in ScoverageITest <<= (resourceGenerators in Test),
-        unmanagedResources in ScoverageITest <<= (unmanagedResources in Test),
-        javacOptions in ScoverageITest <<= (javacOptions in Test),
-        javaOptions in ScoverageITest <<= (javaOptions in Test),
-        fork in ScoverageITest <<= (fork in Test),
-        testOptions in ScoverageITest <<= (testOptions in Test),
+        sources in ScoverageITest <<= (sources in Test),
+        sourceDirectory in ScoverageITest <<= (sourceDirectory in IntegrationTest),
+        resourceDirectory in ScoverageITest <<= (resourceDirectory in IntegrationTest),
+        resourceGenerators in ScoverageITest <<= (resourceGenerators in IntegrationTest),
+        unmanagedResources in ScoverageITest <<= (unmanagedResources in IntegrationTest),
+        javaOptions in ScoverageITest <<= (javaOptions in IntegrationTest),
+        javacOptions in ScoverageITest <<= (javacOptions in IntegrationTest),
+        fork in ScoverageITest <<= (fork in IntegrationTest),
+        testOptions in ScoverageITest <<= (testOptions in IntegrationTest),
 
         externalDependencyClasspath in ScoverageCompile <<= Classpaths
           .concat(externalDependencyClasspath in ScoverageCompile, externalDependencyClasspath in Compile),
         externalDependencyClasspath in ScoverageTest <<= Classpaths
           .concat(externalDependencyClasspath in ScoverageTest, externalDependencyClasspath in Test),
         externalDependencyClasspath in ScoverageITest <<= Classpaths
-          .concat(externalDependencyClasspath in ScoverageTest, externalDependencyClasspath in Test),
+          .concat(externalDependencyClasspath in ScoverageTest, externalDependencyClasspath in IntegrationTest),
 
         internalDependencyClasspath in ScoverageCompile <<= (internalDependencyClasspath in Compile),
         internalDependencyClasspath in ScoverageTest <<= (internalDependencyClasspath in Test, internalDependencyClasspath in ScoverageTest, classDirectory in Compile) map {
           (testDeps, scoverageDeps, oldClassDir) =>
             scoverageDeps ++ testDeps.filter(_.data != oldClassDir)
         },
-        internalDependencyClasspath in ScoverageITest <<= (internalDependencyClasspath in Test, internalDependencyClasspath in ScoverageITest, classDirectory in Compile) map {
+        internalDependencyClasspath in ScoverageITest <<= (internalDependencyClasspath in IntegrationTest, internalDependencyClasspath in ScoverageITest, classDirectory in Compile) map {
           (testDeps, scoverageDeps, oldClassDir) =>
             scoverageDeps ++ testDeps.filter(_.data != oldClassDir)
         },
