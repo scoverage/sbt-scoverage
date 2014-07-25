f2a4104841125c13ad0de25c81a46b2fe27d749e
sksamuel <sam@sksamuel.com>
2014-07-25 12:13:57 +0100

    Reverting previous ballsup

--- src/main/scala/scoverage/ScoverageSbtPlugin.scala
+++ src/main/scala/scoverage/ScoverageSbtPlugin.scala
@@ -13,29 +13,32 @@ class ScoverageSbtPlugin extends sbt.Plugin {
   val ScoverageVersion = "0.99.7"
 
   object ScoverageKeys {
-    lazy val excludedPackages = SettingKey[String]("scoverage-excluded-packages")
-    lazy val minimumCoverage = SettingKey[Double]("scoverage-minimum-coverage")
-    lazy val failOnMinimumCoverage = SettingKey[Boolean]("scoverage-fail-on-minimum-coverage")
-    lazy val highlighting = SettingKey[Boolean]("scoverage-highlighting", "enables range positioning for highlighting")
-    lazy val scoverageReport = taskKey[Unit]("scoverage-report")
-    lazy val scoverageMultiReport = taskKey[Unit]("scoverage-multireport")
+    val excludedPackages = SettingKey[String]("scoverage-excluded-packages")
+    val minimumCoverage = SettingKey[Double]("scoverage-minimum-coverage")
+    val failOnMinimumCoverage = SettingKey[Boolean]("scoverage-fail-on-minimum-coverage")
+    val highlighting = SettingKey[Boolean]("scoverage-highlighting", "enables range positioning for highlighting")
   }
 
   import ScoverageKeys._
 
   lazy val ScoverageCompile: Configuration = config("scoverage")
   lazy val ScoverageTest: Configuration = config("scoverage-test") extend ScoverageCompile
-  lazy val ScoverageITest: Configuration = config("scoverage-itest") extend ScoverageTest
 
   lazy val instrumentSettings: Seq[Setting[_]] = {
     inConfig(ScoverageCompile)(Defaults.compileSettings) ++
       inConfig(ScoverageTest)(Defaults.testSettings) ++
-      inConfig(ScoverageITest)(Defaults.itSettings) ++
       Seq(
         ivyConfigurations ++= Seq(ScoverageCompile.hide, ScoverageTest.hide),
         libraryDependencies += {
           OrgScoverage % (ScalacArtifact + "_" + scalaBinaryVersion.value) % ScoverageVersion % ScoverageCompile.name
         },
+        sources in ScoverageCompile <<= (sources in Compile),
+        sourceDirectory in ScoverageCompile <<= (sourceDirectory in Compile),
+        resourceDirectory in ScoverageCompile <<= (resourceDirectory in Compile),
+        resourceGenerators in ScoverageCompile <<= (resourceGenerators in Compile),
+        excludedPackages in ScoverageCompile := "",
+        javacOptions in ScoverageCompile <<= (javacOptions in Compile),
+        javaOptions in ScoverageCompile <<= (javaOptions in Compile),
 
         minimumCoverage := 0, // default is no minimum
         failOnMinimumCoverage := false,
@@ -55,102 +58,21 @@ class ScoverageSbtPlugin extends sbt.Plugin {
             }
         },
 
-        scoverageReport := {
-          val cross = crossTarget.value
-          val compileSourceDirectory = (scalaSource in Compile).value
-          val baseDir = (baseDirectory in Compile).value
-          val log = sbt.Keys.streams.value.log
-
-          log.info(s"[scoverage] Waiting for measurement data to sync...")
-          Thread.sleep(3000) // have noticed some delay in writing, hacky but works
-
-          val dataDir = cross / "/scoverage-data"
-          val coberturaDir = cross / "coverage-report"
-          val scoverageDir = cross / "scoverage-report"
-          coberturaDir.mkdirs()
-          scoverageDir.mkdirs()
-
-          val coverageFile = IOUtils.coverageFile(dataDir)
-          val measurementFiles = IOUtils.findMeasurementFiles(dataDir)
-
-          log.info(s"[scoverage] Reading scoverage instrumentation [$coverageFile]")
-          log.info(s"[scoverage] Reading scoverage measurements...")
-
-          val coverage = IOUtils.deserialize(coverageFile)
-          val measurements = IOUtils.invoked(measurementFiles)
-          coverage.apply(measurements)
-
-          log.info(s"[scoverage] Generating Cobertura report [${coberturaDir.getAbsolutePath}/cobertura.xml]")
-          new CoberturaXmlWriter(baseDir, coberturaDir).write(coverage)
-
-          log.info(s"[scoverage] Generating XML report [${scoverageDir.getAbsolutePath}/scoverage.xml]")
-          new ScoverageXmlWriter(compileSourceDirectory, scoverageDir, false).write(coverage)
-          new ScoverageXmlWriter(compileSourceDirectory, scoverageDir, true).write(coverage)
-
-          log.info(s"[scoverage] Generating HTML report [${scoverageDir.getAbsolutePath}/index.html]")
-          new ScoverageHtmlWriter(compileSourceDirectory, scoverageDir).write(coverage)
-
-          log.info("[scoverage] Reports completed")
-          val min = minimumCoverage.value
-
-          // check for default minimum
-          if (min > 0) {
-            if (min > coverage.statementCoveragePercent) {
-              log.error(s"[scoverage] Coverage is below minimum [${coverage.statementCoverageFormatted}% < $min%]")
-              if (failOnMinimumCoverage.value) {
-                // todo better way to fail an SBT build ?
-                throw new RuntimeException("Coverage minimum was not reached. Failing build.")
-              }
-            } else {
-              log.info(s"[scoverage] Coverage is above minimum [${coverage.statementCoverageFormatted}% > $min%]")
-            }
-          }
-
-          log.info(s"[scoverage] All done. Coverage was [${coverage.statementCoverageFormatted}%]")
-        },
-
-        scoverageMultiReport := {
-
-        },
-
         scalacOptions in ScoverageCompile ++= (if (highlighting.value) List("-Yrangepos") else Nil),
-
-        sources in ScoverageCompile <<= (sources in Compile),
-        sourceDirectory in ScoverageCompile <<= (sourceDirectory in Compile),
-        resourceDirectory in ScoverageCompile <<= (resourceDirectory in Compile),
-        resourceGenerators in ScoverageCompile <<= (resourceGenerators in Compile),
-        unmanagedResources in ScoverageCompile <<= (unmanagedResources in Compile),
-        javaOptions in ScoverageCompile <<= (javaOptions in Compile),
-        javacOptions in ScoverageCompile <<= (javacOptions in Compile),
-        fork in ScoverageCompile <<= (fork in Compile),
-        excludedPackages in ScoverageCompile := "",
 
         sources in ScoverageTest <<= (sources in Test),
         sourceDirectory in ScoverageTest <<= (sourceDirectory in Test),
         resourceDirectory in ScoverageTest <<= (resourceDirectory in Test),
         resourceGenerators in ScoverageTest <<= (resourceGenerators in Test),
         unmanagedResources in ScoverageTest <<= (unmanagedResources in Test),
-        javaOptions in ScoverageTest <<= (javaOptions in Test),
         javacOptions in ScoverageTest <<= (javacOptions in Test),
+        javaOptions in ScoverageTest <<= (javaOptions in Test),
         fork in ScoverageTest <<= (fork in Test),
-        testOptions in ScoverageTest <<= (testOptions in Test),
-
-        sources in ScoverageITest <<= (sources in Test),
-        sourceDirectory in ScoverageITest <<= (sourceDirectory in IntegrationTest),
-        resourceDirectory in ScoverageITest <<= (resourceDirectory in IntegrationTest),
-        resourceGenerators in ScoverageITest <<= (resourceGenerators in IntegrationTest),
-        unmanagedResources in ScoverageITest <<= (unmanagedResources in IntegrationTest),
-        javaOptions in ScoverageITest <<= (javaOptions in IntegrationTest),
-        javacOptions in ScoverageITest <<= (javacOptions in IntegrationTest),
-        fork in ScoverageITest <<= (fork in IntegrationTest),
-        testOptions in ScoverageITest <<= (testOptions in IntegrationTest),
 
         externalDependencyClasspath in ScoverageCompile <<= Classpaths
           .concat(externalDependencyClasspath in ScoverageCompile, externalDependencyClasspath in Compile),
         externalDependencyClasspath in ScoverageTest <<= Classpaths
           .concat(externalDependencyClasspath in ScoverageTest, externalDependencyClasspath in Test),
-        externalDependencyClasspath in ScoverageITest <<= Classpaths
-          .concat(externalDependencyClasspath in ScoverageTest, externalDependencyClasspath in IntegrationTest),
 
         internalDependencyClasspath in ScoverageCompile <<= (internalDependencyClasspath in Compile),
         internalDependencyClasspath in ScoverageTest <<= (internalDependencyClasspath in Test, internalDependencyClasspath in ScoverageTest, classDirectory in Compile) map {
@@ -158,17 +80,84 @@ class ScoverageSbtPlugin extends sbt.Plugin {
             scoverageDeps ++ testDeps.filter(_.data != oldClassDir)
         },
 
-        test in ScoverageTest := {
-          (test in Test).value
-          scoverageReport.value
-        },
-        test in ScoverageITest := {
-          (test in IntegrationTest).value
-          scoverageReport.value
-        },
+        testOptions in ScoverageTest <<= (testOptions in Test),
+        testOptions in ScoverageTest <+= testsCleanup,
 
-        // copy the test task into compile so we can do scoverage:test instead of scoverage-test:test etc
+        // copy the test task into compile so we can do scoverage:test instead of scoverage-test:test
         test in ScoverageCompile <<= (test in ScoverageTest)
       )
+  }
+
+  /** Generate hook that is invoked after the tests have executed. */
+  def testsCleanup = {
+    (crossTarget in ScoverageTest,
+      baseDirectory in Compile,
+      scalaSource in Compile,
+      definedTests in ScoverageTest,
+      minimumCoverage in ScoverageTest,
+      failOnMinimumCoverage in ScoverageTest,
+      streams in Global) map {
+      (crossTarget,
+       baseDirectory,
+       compileSourceDirectory,
+       definedTests,
+       min,
+       failOnMin,
+       s) =>
+        if (definedTests.isEmpty) {
+          Tests.Cleanup {
+            () => {}
+          }
+        } else {
+          Tests.Cleanup {
+            () =>
+
+              s.log.info(s"[scoverage] Waiting for measurement data to sync...")
+              Thread.sleep(2000) // have noticed some delay in writing, hacky but works
+
+              val dataDir = crossTarget / "/scoverage-data"
+              val coberturaDir = crossTarget / "coverage-report"
+              val reportDir = crossTarget / "scoverage-report"
+              coberturaDir.mkdirs()
+              reportDir.mkdirs()
+
+              val coverageFile = IOUtils.coverageFile(dataDir)
+              val measurementFiles = IOUtils.findMeasurementFiles(dataDir)
+
+              s.log.info(s"[scoverage] Reading scoverage instrumentation [$coverageFile]")
+              s.log.info(s"[scoverage] Reading scoverage measurements...")
+
+              val coverage = IOUtils.deserialize(coverageFile)
+              val measurements = IOUtils.invoked(measurementFiles)
+              coverage.apply(measurements)
+
+              s.log.info(s"[scoverage] Generating Cobertura report [${coberturaDir.getAbsolutePath}/cobertura.xml]")
+              new CoberturaXmlWriter(baseDirectory, coberturaDir).write(coverage)
+
+              s.log.info(s"[scoverage] Generating XML report [${reportDir.getAbsolutePath}/scoverage.xml]")
+              new ScoverageXmlWriter(compileSourceDirectory, reportDir, false).write(coverage)
+              new ScoverageXmlWriter(compileSourceDirectory, reportDir, true).write(coverage)
+
+              s.log.info(s"[scoverage] Generating HTML report [${reportDir.getAbsolutePath}/index.html]")
+              new ScoverageHtmlWriter(compileSourceDirectory, reportDir).write(coverage)
+
+              s.log.info("[scoverage] Reports completed")
+
+              // check for default minimum
+              if (min > 0) {
+                if (min > coverage.statementCoveragePercent) {
+                  s.log.error(s"[scoverage] Coverage is below minimum [${coverage.statementCoverageFormatted}% < $min%]")
+                  if (failOnMin)
+                    throw new RuntimeException("Coverage minimum was not reached")
+                } else {
+                  s.log.info(s"[scoverage] Coverage is above minimum [${coverage.statementCoverageFormatted}% > $min%]")
+                }
+              }
+
+              s.log.info(s"[scoverage] All done. Coverage was [${coverage.statementCoverageFormatted}%]")
+              ()
+          }
+        }
+    }
   }
 }
