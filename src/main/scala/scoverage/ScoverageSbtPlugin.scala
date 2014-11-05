5fb9c9d2edaa7c155848f84862ab4937e5c8df11
sksamuel <sam@sksamuel.com>
2014-11-05 22:37:06 +0000

    Release 0.99.11

--- src/main/scala/scoverage/ScoverageSbtPlugin.scala
+++ src/main/scala/scoverage/ScoverageSbtPlugin.scala
@@ -10,7 +10,7 @@ class ScoverageSbtPlugin extends sbt.Plugin {
 
   val OrgScoverage = "org.scoverage"
   val ScalacArtifact = "scalac-scoverage-plugin"
-  val ScoverageVersion = "0.99.10"
+  val ScoverageVersion = "0.99.11"
 
   object ScoverageKeys {
     val excludedPackages = SettingKey[String]("scoverage-excluded-packages")
