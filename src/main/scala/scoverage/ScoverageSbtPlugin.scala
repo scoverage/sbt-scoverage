4af7a7af3d7f60a4b73e57ae954fce2b5b30baa0
sksamuel <sam@sksamuel.com>
2015-06-04 22:58:20 +0100

    #106 Set scoverage deps to intransitive

--- src/main/scala/scoverage/ScoverageSbtPlugin.scala
+++ src/main/scala/scoverage/ScoverageSbtPlugin.scala
@@ -91,8 +91,8 @@ class ScoverageSbtPlugin extends sbt.AutoPlugin {
     aggregate in coverageAggregate := false,
 
     libraryDependencies ++= Seq(
-      OrgScoverage % (ScalacRuntimeArtifact + "_" + scalaBinaryVersion.value) % ScoverageVersion % "provided",
-      OrgScoverage % (ScalacPluginArtifact + "_" + scalaBinaryVersion.value) % ScoverageVersion % "provided"
+      OrgScoverage % (ScalacRuntimeArtifact + "_" + scalaBinaryVersion.value) % ScoverageVersion % "provided" intransitive(),
+      OrgScoverage % (ScalacPluginArtifact + "_" + scalaBinaryVersion.value) % ScoverageVersion % "provided" intransitive()
     ),
 
     scalacOptions in(Compile, compile) ++= {
